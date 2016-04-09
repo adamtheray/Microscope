d8913ea43ac6400f58973a0dc05999cf5b0389c0
adamtheray <aray@pronetindy.com>
2016-04-09 12:19:57 -0400

    End Chapter 4

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,17 +1,14 @@
-var postsData=[
-  {
+if (Posts.find().count() === 0) {
+  Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
+  });
+  Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com'
-  },
-  {
+  });
+  Posts.insert({
     title: 'The Meteor Book',
-    url:'http://themeteorbook.com'
-  }
-];
-Template.postsList.helpers({
-  posts: postsData
-});
+    url: 'http://themeteorbook.com'
+  });
+}
